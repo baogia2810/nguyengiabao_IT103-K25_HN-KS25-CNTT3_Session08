229a03b93061dd7e33a9fe35110b0d18032f5a6f
baogia2810 <bankaijun2880@gmail.com>
2026-02-25 07:55:31 +0700

    upload bài 1

---
+++ Bai_1.js
@@ -0,0 +1,17 @@
+const players = [
+  "Messi - Forward",
+  "Ronaldo - Forward",
+  "Neymar - Forward",
+  "De Bruyne - Midfielder",
+  "Kante - Midfielder",
+  "Van Dijk - Defender",
+  "Alisson - Goalkeeper",
+];
+
+function displayPlayers(players) {
+  for (let i = 0; i < players.length; i++) {
+    console.log(players[i]);
+  }
+}
+
+displayPlayers(players);
